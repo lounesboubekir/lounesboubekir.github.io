666f58cc3f1cf9d95e15d29871b758d1800dcefa
lounesbbkr <99367325+lounesbbkr@users.noreply.github.com>
2024-05-08 00:09:39 +0100

    img Changes

--- src/js/solchlenges.js
+++ src/js/solchlenges.js
@@ -1,27 +1,4 @@
-function solveftc(event) {
-    var userInput;
-    var correctAnswers = ["china", "brazil", "palectine"]; 
-    var correctCount = 0; 
-
-    for (var i = 0; i < 3; i++) {
-        userInput = prompt("Please enter country #" + (i + 1) + ":");
-        if (userInput !== null) {
-            if (correctAnswers.includes(userInput.toLowerCase())) {
-                correctCount++;
-            }
-        }
-    }
-
-    if (correctCount === 3) {
-        // Create a new window for the image
-        var popup = window.open("", "", "width=400,height=400");
-        // Show the gif image in the new window
-        popup.document.write("<img src='src/imgs/logos/congrats.gif'>");
-        alert("Correct answers!");
-    } else {
-        alert("Incorrect answers. Please try again.");
-    }
+function coming(event)
+{
+    alert("Coming soon!");
 }
-
-
-
